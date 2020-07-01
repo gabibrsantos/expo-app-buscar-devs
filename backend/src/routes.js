60ff621e93706b15d939164fbca779f1eb26655c
gabriela <gabibrsantos@gmail.com>
2020-07-01 12:55:48 -0300

    first commit

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,8 +3,11 @@ const DevController = require('./controllers/DevController');
 const SearchController = require('./controllers/SearchController');
 const routes = Router();
 
+/** Web, listar os devs(get) e cadastrar(post) */
 routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
+
+/** Mobile, encontrar devs(get) */
 routes.get('/search', SearchController.index);
 
 module.exports = routes;
